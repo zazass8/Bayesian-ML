76d49407af847ddf2131c6691bdc3a8fbb04cb98
zazass8 <constantinosazas@hotmail.com>
2023-10-04 11:31:40 +0200

    Last commit

--- algorithms/hmc_Lab.py
+++ algorithms/hmc_Lab.py
@@ -1,38 +1,3 @@
-#
-# hmc_Lab for CM50268 Bayesian Machine Learning
-# April 2022
-# Python port of Radford Neal "one_step" code - original comments below
-#
-# SIMPLE IMPLEMENTATION OF HAMILTONIAN MONTE CARLO.
-#
-# Radford M. Neal, 2010.
-#
-# This program appears in Figure 2 of "MCMC using Hamiltonian dynamics",
-# to appear in the Handbook of Markov Chain Monte Carlo.
-#
-# The arguments to the HMC function are as follows:
-#
-#   U          A function to evaluate minus the log of the density of the
-#              distribution to be sampled, plus any constant - ie, the
-#              "potential energy".
-#
-#   grad_U     A function to evaluate the gradient of U.
-#
-#   epsilon    The stepsize to use for the leapfrog steps.
-#
-#   L          The number of leapfrog steps to do to propose a new state.
-#
-#   current_q  The current state (position variables only).
-#
-# Momentum variables are sampled from independent standard normal
-# distributions within this function.  The value return is the vector
-# of new position variables (equal to current_q if the endpoint of the
-# trajectory was rejected).
-#
-# This function was written for illustrative purposes.  More elaborate
-# implementations of this basic HMC method and various variants of HMC
-# are available from my web page, http://www.cs.utoronto.ca/~radford/
-#
 import numpy as np
 import time
 
@@ -46,17 +11,17 @@ def gradient_check(x0, fn_error, fn_grad, *args):
     :param fn_grad: Function object, the gradient of the error wrt x
     :param args: Any optional arguments to be passed to both functions
     """
-    #
+    
     delt = 1e-6
     alert_rel_red = 4
     alert_rel_yellow = 6
-    #
+    
     N = len(x0)
-    #
+    
     x = x0.copy()
     grad = fn_grad(x, *args)
     gnum = np.empty(N)
-    #
+    
     print('{0:13s} {1:13s} {2:13s} Acc.'.format('Calc.', 'Numeric', 'Delta'))
     for n in range(N):
         x[n] += delt
@@ -64,21 +29,21 @@ def gradient_check(x0, fn_error, fn_grad, *args):
         x[n] -= 2*delt
         fminus = fn_error(x, *args)
         x[n] += delt
-        #
+        
         gnum[n] = (fplus-fminus)/(2*delt)
         differ = gnum[n]-grad[n]
         if differ == 0:
             one_part_in = 16
         else:
             one_part_in = int(np.log10(np.abs(gnum[n] / differ)))+1
-        #
+        
         if one_part_in <= alert_rel_red:
             trm_col = '91'
         elif one_part_in <= alert_rel_yellow:
             trm_col = '93'
         else:
             trm_col = '37'
-        #
+        
         coldiff = '\x1b[{0}m{1: 10e}\x1b[0m'.format(trm_col, differ)
         print('{0:> 12g}  {1:> 12g}  {2:12} {3:3d}'.
               format(grad[n], gnum[n], coldiff, one_part_in))
